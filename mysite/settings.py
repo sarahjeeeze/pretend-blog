7bb81b7d087fc6030dc39466be2e680269fed4f3
sarahjeeeze <peggi2@hotmail.com>
2022-05-11 19:55:29 +0100

    login

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#  when the login page is accessed directly, it will redirect a successful login to the top-level index
+LOGIN_REDIRECT_URL = '/'
+
